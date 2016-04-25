0bf967149ca096bca3b03c7441bfca9714998267
Anh Le <lequocanh88@gmail.com>
2016-04-25 23:48:49 +0700

    try LearnYouNode workshop on nodeschool.io

---
+++ LearnYouNode/program.js
@@ -0,0 +1,13 @@
+//Bai 1
+//console.log('HELLO WORLD');
+
+//Bai 2
+//console.log(process.argv); //process là 1 object trong node, argv là attribute dạng mảng của process, chứa toàn bộ command line
+// var array = process.argv;
+// var sum = 0;
+// for (var i = 2; i<array.length; i++) {
+// 	sum += Number(array[i]);
+// }
+// console.log(sum);
+
+//Bai 3
