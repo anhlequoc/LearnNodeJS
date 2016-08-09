7b2ebe219b5abc200c44a0b0180d9bdf4b4bc04e
Anh Le <lequocanh88@gmail.com>
2016-08-09 23:00:36 +0700

    start learning chapter 7

--- streamAdventure/duplexer.js
+++ streamAdventure/duplexer.js
@@ -1,6 +1,6 @@
 //Chua hieu lam nen tam thoi copy solution
 var spawn = require('child_process').spawn;
-var duplexer = require('duplexer2');
+var duplexer = require('duplexer2'); console.log(typeof(duplexer));
 
 module.exports = function(cmd, args){
 	var ps = spawn(cmd, args);
